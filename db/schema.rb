8f2c95a5e1cb55aa601381350c3efbca691e23d9
floriansaubin <saubinflorian@gmail.com>
2021-03-15 15:51:26 +0100

    websocket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_135137) do
+ActiveRecord::Schema.define(version: 2021_03_15_143237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(version: 2021_03_15_135137) do
     t.bigint "offers_id"
     t.index ["offers_id"], name: "index_bookings_on_offers_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -52,5 +68,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_135137) do
 
   add_foreign_key "bookings", "offers", column: "offers_id"
   add_foreign_key "bookings", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "offers", "users"
 end
