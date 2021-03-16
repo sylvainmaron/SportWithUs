61074b7ef36f7255df9cda0f9d50d25e0c306bff
floriansaubin <saubinflorian@gmail.com>
2021-03-16 10:43:34 +0100

    hammer.js installed

---
+++ app/javascript/custom/hammer.js
@@ -0,0 +1,13 @@
+// Get a reference to an element.
+// const square = document.querySelector('.square');
+
+// // Create an instance of Hammer with the reference.
+// const hammer = new Hammer(square);
+
+// // Subscribe to a quick start event: press, tap, or doubletap.
+// // For a full list of quick start events, read the documentation.
+// hammer.on('doubletap', function(e) {
+//   e.target.classList.toggle('expand');
+//   console.log("You're doubletaping me!");
+//   console.log(e);
+// });
