1c11162763a609bd8390e2d6ad8572e3430d08fb
floriansaubin <saubinflorian@gmail.com>
2021-03-16 16:15:18 +0100

    WIP tinder swip

--- app/javascript/custom/hammer.js
+++ app/javascript/custom/hammer.js
@@ -1,14 +1,52 @@
-const cards = document.querySelectorAll(".card");
-const play = document.querySelector(".play");
-const pass = document.querySelector(".pass");
+import 'hammerjs'
 
-cards.forEach((card) => {
-  console.log(card)
-  let hammerTime = new Hammer(card)
-  hammerTime.on('tap', function(e){
-    let posX = e.deltaX
-    let posY = e.deltaY
-    card.style.transform = `translateX(${posX}px) translateY(${posY}px)`
-  })
- 
-})
+const initHammer = () => {
+  const cards = document.querySelectorAll(".card");
+  const play = document.querySelector(".play");
+  const pass = document.querySelector(".pass");
+  
+  const maxAngle = 25;
+  const smooth = 0.3;
+  const threshold = 42;
+  const thresholdMatch = 150;
+  cards.forEach(setupDragAndDrop);
+  
+  function setupDragAndDrop(card) {
+    const hammertime = new Hammer(card);
+  
+    hammertime.on('pan', function (e) {
+      card.classList.remove('card--back');
+      let posX = e.deltaX;
+      let posY = Math.max(0, Math.abs(posX * smooth) - 25);
+      let angle = Math.min(Math.abs(e.deltaX * smooth / 100), 1) * maxAngle;
+      if (e.deltaX < 0) {
+        angle *= -1;
+      }
+  
+      card.style.transform = `translateX(${posX}px) translateY(${posY}px) rotate(${angle}deg)`;
+      play.style.opacity = 0;
+      pass.style.opacity = 0;
+      if (posX > thresholdMatch) {
+        card.classList.add('card--matching');
+        play.style.opacity = 1;
+      } else if (posX < -thresholdMatch) {
+        card.classList.add('card--nexting');
+        pass.style.opacity = 1;
+      }
+  
+      if (e.isFinal) {
+        card.style.transform = ``;
+        if (posX > thresholdMatch) {
+          card.classList.add('card--match');
+        } else if (posX < -thresholdMatch) {
+          card.classList.add('card--next');
+        } else {
+          card.classList.add('card--back');
+        }
+      }
+    });
+  }
+}  
+
+
+export { initHammer }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("hammerjs")
-require("custom/hammer")
+//= require hammer
+import { initHammer } from "custom/hammer"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -34,4 +34,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initChatroomCable()
+  initHammer()
 });
