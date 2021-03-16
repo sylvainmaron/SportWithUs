e3d9846efa20797e93b3fdb4cc1b12c3e343f2a8
floriansaubin <saubinflorian@gmail.com>
2021-03-16 13:33:31 +0100

    WIP swipe

--- app/javascript/custom/hammer.js
+++ app/javascript/custom/hammer.js
@@ -1,16 +1,14 @@
-// Get a reference to an element.
-// const square = document.querySelector('.square');
-
-// // Create an instance of Hammer with the reference.
-// const hammer = new Hammer(square);
-
-// // Subscribe to a quick start event: press, tap, or doubletap.
-// // For a full list of quick start events, read the documentation.
-// hammer.on('doubletap', function(e) {
-//   e.target.classList.toggle('expand');
-//   console.log("You're doubletaping me!");
-//   console.log(e);
-// });
-
+const cards = document.querySelectorAll(".card");
 const play = document.querySelector(".play");
 const pass = document.querySelector(".pass");
+
+cards.forEach((card) => {
+  console.log(card)
+  let hammerTime = new Hammer(card)
+  hammerTime.on('tap', function(e){
+    let posX = e.deltaX
+    let posY = e.deltaY
+    card.style.transform = `translateX(${posX}px) translateY(${posY}px)`
+  })
+ 
+})
