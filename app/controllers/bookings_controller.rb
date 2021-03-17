f9a36de969757e5f79e9a05f5809f0ae00b802fa
floriansaubin <saubinflorian@gmail.com>
2021-03-17 12:58:01 +0100

    WOrk

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,12 +2,14 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(status: :play)
     @offer = Offer.find(params[:offer_id])
-    @booking.offer_id = @offer
+    @booking.offer_id = @offer.id
     @booking.user = current_user
 
+    
     if @booking.save
       redirect_to offer_path(@offer)
     else
+      byebug
       render offers_path
     end
   end
