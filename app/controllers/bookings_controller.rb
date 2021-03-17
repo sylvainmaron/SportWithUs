39142780d899a10fe41600c4da519464792bef23
floriansaubin <saubinflorian@gmail.com>
2021-03-17 12:11:13 +0100

    WIP

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,14 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new(status: :play)
     @offer = Offer.find(params[:offer_id])
     @booking.offer = @offer
     @booking.user = current_user
+
     if @booking.save
       redirect_to offer_path(@offer)
     else
       render offers_path
     end
-  end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:status)
   end
 end
--- app/javascript/custom/fetching.js
+++ app/javascript/custom/fetching.js
@@ -1,17 +1,24 @@
-import { csrfToken } from "@rails/ujs";
-
+import Rails from "@rails/ujs"
 
 const fetchingCard = (card) => {
   if (card.classList.contains("card--matching")) {
-    fetch(`offers/${card.dataset.offerId}/bookings`, {
-      method: "POST", 
-      headers: { "X-CSRF-Token": csrfToken() },
-      body: JSON.stringify({status: 'play'})
+    Rails.ajax({
+      url: `offers/${card.dataset.offerId}/bookings`,
+      type: "POST",
+      success: function(data) {console.log(data)},
+      error: function(data) {}
     })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-      });
+
+
+    // fetch(`offers/${card.dataset.offerId}/bookings`, {
+    //   method: "POST", 
+    //   headers: { "X-CSRF-Token": csrfToken() },
+    //   body: JSON.stringify({status: 'play'})
+    // })
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
   }
 }
 
