94870381da1c208e12eba5a773a98c0cf2f866d1
floriansaubin <saubinflorian@gmail.com>
2021-03-17 12:28:56 +0100

    booking work

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(status: :play)
     @offer = Offer.find(params[:offer_id])
-    @booking.offer = @offer
+    @booking.offer_id = @offer
     @booking.user = current_user
 
     if @booking.save
