557fd720a4674984bb4ad15c7740bf68d6785bc3
floriansaubin <saubinflorian@gmail.com>
2021-03-18 17:52:31 +0100

    wheater fetch ok

--- app/javascript/custom/weather_fetch.js
+++ app/javascript/custom/weather_fetch.js
@@ -1,10 +1,11 @@
 const weatherFetch = () => {
   navigator.geolocation.getCurrentPosition((data) => {
-    console.log(data);
-    fetch(`api.openweathermap.org/data/2.5/weather?lat=${data.coords.latitude}&lon=${data.coords.longitude}&appid=e02bc9970df1fa9c02b6130b832dbec0`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.coords.latitude}&lon=${data.coords.longitude}&appid=e02bc9970df1fa9c02b6130b832dbec0`)
     .then(response => response.json())
     .then((data) => {
+      console.log(data)
     });
   });
-
 }
+
+export { weatherFetch }
