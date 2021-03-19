fd22fbe067998424aa837126e6af32987d698ea7
floriansaubin <saubinflorian@gmail.com>
2021-03-19 11:38:30 +0100

    Chatroom bug

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    @offer = Offer.find(params[:id])
+    @offer.chatroom = @chatroom
     @message = Message.new
   end
 end
