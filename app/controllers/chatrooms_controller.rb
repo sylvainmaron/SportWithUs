122aaf115aad73c8c5022015c10e9557964b432f
floriansaubin <saubinflorian@gmail.com>
2021-03-19 11:48:58 +0100

    WIP chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,6 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    @offer = Offer.find(params[:id])
-    @offer.chatroom = @chatroom
     @message = Message.new
   end
 end
