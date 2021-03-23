4008056fefb5b5b7163a1cedf258fe1aa035fed4
sylvainmaron <sylvain.maron@essca.eu>
2021-03-23 10:03:35 +0100

    ee

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates :nickname, :age, :description, presence: true
 end
