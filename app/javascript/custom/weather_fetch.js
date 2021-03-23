bd6bc6bfb9e6f6319314adcd089e3a013976d6b1
floriansaubin <saubinflorian@gmail.com>
2021-03-23 14:49:37 +0100

    V3

--- app/javascript/custom/weather_fetch.js
+++ app/javascript/custom/weather_fetch.js
@@ -5,17 +5,21 @@ const weatherFetch = () => {
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.coords.latitude}&lon=${data.coords.longitude}&units=metric&appid=e02bc9970df1fa9c02b6130b832dbec0`)
       .then(response => response.json())
       .then((data) => {
+        console.log(data)
         const apiData = data.weather[0];
         const display = ` <h2>Your local weather</h2>
           <p>${apiData.main} - ${apiData.description}</p>
           <p class="weather-celsius">${data.main.temp}°C</p>
           <hr size="8" width="90%" color="#39ac2f"> 
           <div class= "weather-temp">
-            <div class="temp-deg">
+            <div class="temp-deg1">
               <p><i class="fas fa-tint"></i> ${data.main.humidity}%</p>
               <p><i class="fas fa-wind"></i> ${data.wind.speed}m/s</p>
             </div>
-            <img src="http://openweathermap.org/img/wn/${apiData.icon}@2x.png">
+            <div class="temp-deg2">
+              <p><i class="fas fa-temperature-high"></i> ${data.main.temp_max}°C</p>
+              <p><i class="fas fa-temperature-low"></i> ${data.main.temp_min}°C</p>
+            </div>            
           </div>`
         weather.insertAdjacentHTML("beforeend", display);
       });
