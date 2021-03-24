67e768f706bafc744e19f5dc2eb1a5f89127557c
floriansaubin <saubinflorian@gmail.com>
2021-03-24 09:14:28 +0100

    modal move fixed

--- app/javascript/packs/plugins/modal.js
+++ app/javascript/packs/plugins/modal.js
@@ -1,19 +1,20 @@
+const toggleTabs = (e) => {
+  const parent = document.querySelector(".main-modal");
+  e.addEventListener("click", (event) => {
+    if (event.currentTarget.classList.contains("active")){
+      return false
+    }
+    parent.classList.toggle('move-right');
+    document.querySelector(".onglets li.active").classList.remove("active")
+    event.currentTarget.classList.add("active");
+  });
+}
+
 const modalle = () => {
 
   const onglets = document.querySelectorAll(".onglets li");
   if (onglets) { 
-    onglets.forEach(toggleTabs)
-    
-    function toggleTabs(e) {
-      const parent = document.querySelector(".main-modal");
-      e.addEventListener("click", (event) => {
-        if (event.currentTarget.classList.contains("active")){
-          return false
-        }
-        parent.classList.toggle('move-right');
-        event.currentTarget.classList.toggle("active");
-      });
-    }
+    onglets.forEach(toggleTabs)    
   }
 }
 export { modalle }
