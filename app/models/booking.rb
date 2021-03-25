aeb8e3b9fc70794a312d52799f52fec4f60c0f8d
sylvainmaron <sylvain.maron@essca.eu>
2021-03-25 15:48:41 +0100

    e

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  def is_coming?
+    offer.play_time > Date.today
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # validates :nickname, :age, :description, presence: true
+
+  def booking_coming_count
+    coming_bookings = bookings.filter do |booking|
+      booking.is_coming?
+    end
+    coming_bookings.length
+  end
 end
